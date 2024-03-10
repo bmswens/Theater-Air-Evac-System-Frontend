d22794c696d439cc23de29b9481692af046a4610
bmswens <bmswens@gmail.com>
2024-03-10 22:06:06 +0000

    resolves #8

--- src/content/documentCards/AF3899Card.js
+++ src/content/documentCards/AF3899Card.js
@@ -17,6 +17,7 @@ import ThirtyEightNintyNineForm from '../../forms/3899/ThirtyEightNinetyNineForm
 import AF3899A from '../../forms/3899/AF3899A';
 import useStorage from '../../api/useStorage';
 import AF3899D from '../../forms/3899/AF3899D';
+import AF3899I from '../../forms/3899/AF3899I';
 
 function AF3899Card(props) {
     const {
@@ -28,6 +29,7 @@ function AF3899Card(props) {
     const [open, setOpen] = React.useState(false)
     const [alphaOpen, setAlphaOpen] = React.useState(false)
     const [deltaOpen, setDeltaOpen] = React.useState(false)
+    const [iotaOpen, setIotaOpen] = React.useState(false)
     const [docs, setDocs] = useStorage(`${dodid}-documents`, [])
     const [patients] = useStorage("patients", [])
     const doc = docs[index]
@@ -50,6 +52,14 @@ function AF3899Card(props) {
     function addDeltaEntry(entry) {
         let tempDoc = {...doc, af3899d: doc.af3899d || []}
         tempDoc.af3899d = [...tempDoc.af3899d, entry]
+        let tempDocs = [...docs]
+        tempDocs.splice(index, 1, tempDoc)
+        setDocs(tempDocs)
+    }
+
+    function addIotaEntry(entry) {
+        let tempDoc = {...doc, af3899i: doc.af3899i || []}
+        tempDoc.af3899i = [...tempDoc.af3899i, entry]
         let tempDocs = [...docs]
         tempDocs.splice(index, 1, tempDoc)
         setDocs(tempDocs)
@@ -96,7 +106,7 @@ function AF3899Card(props) {
                         title="AF Form 3899I"
                     >
                         <IconButton
-                            
+                            onClick={() => setIotaOpen(true)}
                         >
                             <VaccinesIcon fontSize="large" />
                         </IconButton>
@@ -129,6 +139,12 @@ function AF3899Card(props) {
                 close={() => setDeltaOpen(false)}
                 entries={doc.af3899d || []}
                 addEntry={addDeltaEntry}
+            />
+            <AF3899I
+                open={iotaOpen}
+                close={() => setIotaOpen(false)}
+                entries={doc.af3899i || []}
+                addEntry={addIotaEntry}
             />
         </Grid>
     )
---
+++ src/forms/3899/AF3899I.js
@@ -0,0 +1,149 @@
+// React
+import React from 'react'
+
+// MUI
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Divider, Stack, TextField } from '@mui/material'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+
+const blankEntry = {
+    datetime: new Date(),
+    medication: '',
+    dose: '',
+    initials1: '',
+    initials2: ''
+}
+
+function EntryTable(props) {
+    const { rows } = props
+    const columns = [
+        {
+            field: "datetime",
+            headerName: "Datetime",
+            flex: 1
+        },
+        {
+            field: "medication",
+            headerName: "Medication",
+            flex: 1
+        },
+        {
+            field: "dose",
+            headerName: "Dosage",
+            flex: 1
+        },
+        {
+            field: "initials1",
+            headerName: "Initials",
+            flex: 1
+        },
+        {
+            field: "initials2",
+            headerName: "Initials",
+            flex: 1
+        },
+    ]
+
+    const finalRows = rows.map((row, index) => { return { ...row, id: index } })
+
+    return (
+        <Box sx={{ width: "100%", height: 400 }}>
+            <DataGrid
+                columns={columns}
+                rows={finalRows}
+                slots={{ toolbar: GridToolbar }}
+            />
+        </Box>
+    )
+
+}
+
+function AF3899I(props) {
+
+    const {
+        open,
+        close,
+        entries,
+        addEntry
+    } = props
+    console.log(props)
+    const [data, setData] = React.useState(blankEntry)
+
+    function handleClose() {
+        setData(blankEntry)
+        close()
+    }
+
+    function submit() {
+        addEntry(data)
+        setData(blankEntry)
+    }
+
+    return (
+        <Dialog
+            open={open}
+            onClose={close}
+            maxWidth="lg"
+            fullWidth
+            scroll="body"
+        >
+            <DialogTitle align="center">
+                AF Form 3899I
+            </DialogTitle>
+            <DialogContent>
+                <Stack spacing={1} sx={{ marginTop: 1 }}>
+                    <EntryTable
+                        rows={entries}
+                    />
+                    <Divider>
+                        New Entry
+                    </Divider>
+                    <Stack spacing={1} direction="row">
+                        <TextField
+                            fullWidth
+                            label="Medication"
+                            value={data.medication}
+                            onChange={event => setData({ ...data, medication: event.target.value })}
+                        />
+                        <TextField
+                            fullWidth
+                            label="Dosage"
+                            value={data.dose}
+                            onChange={event => setData({ ...data, dose: event.target.value })}
+                        />
+                        <TextField
+                            fullWidth
+                            label="Initials"
+                            value={data.initials1}
+                            onChange={event => setData({ ...data, initials1: event.target.value })}
+                        />
+                        <TextField
+                            fullWidth
+                            label="Initials"
+                            value={data.initials2}
+                            onChange={event => setData({ ...data, initials2: event.target.value })}
+                        />
+                    </Stack>
+                    <Stack spacing={1} direction="row-reverse">
+                        <Button
+                            variant="contained"
+                            onClick={submit}
+                            disabled={data.initials1.length === 0 || data.initials2.length === 0}
+                        >
+                            Submit
+                        </Button>
+                    </Stack>
+                </Stack>
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    onClick={handleClose}
+                >
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+
+}
+
+export default AF3899I
